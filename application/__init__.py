c2f6782241a3ded54021ac1ee759fb797372ffc5
kkekitinisa <kkekitinisa@gmail.com>
2020-07-27 12:22:31 +0000

    registration added.

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from application import routes
