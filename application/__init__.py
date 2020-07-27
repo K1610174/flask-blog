3471ba8e47518a41f4262fe7e549a0090584210f
kkekitinisa <kkekitinisa@gmail.com>
2020-07-27 14:17:19 +0000

    modified

--- application/__init__.py
+++ application/__init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
