9113f8a599506c284eb2bacdafc4112e151c4d68
kkekitinisa <kkekitinisa@gmail.com>
2020-07-27 15:02:46 +0000

    login added

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
 db = SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 
 from application import routes
